5c1377c23c9b0fb68de2c6a5b83de44bc67ddf22
lovleenkaur <lovleenk99@gmail.com>
2023-07-31 17:40:18 -0400

    lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,8 @@ import React from "react";
 
 import Article from "../components/Article";
 
-test("displays the text 'please pass this test'", () => {
+test("displays the text 'Hello from the Header!'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  expect(screen.queryByText("Hello from the Header!")).toBeInTheDocument();
 });
